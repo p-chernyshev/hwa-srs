9f08b7aa3793c7feab07596548e03e12818db700
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2022-04-10 23:46:36 +0500

    init angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'srs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hwa-srs';
+}
