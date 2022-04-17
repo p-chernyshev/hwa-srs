6463568daacda84763352850f39962d8aabf0003
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2022-04-17 21:09:04 +0500

    configure db connection

--- HwaSrsApi/Startup.cs
+++ HwaSrsApi/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using HwaSrsApi.Db;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace HwaSrsApi
 {
@@ -25,6 +20,10 @@ namespace HwaSrsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SrsContext>(builder =>
+            {
+                builder.UseNpgsql(Configuration["DbConnection"]);
+            });
             services.AddControllers();
             services.AddSwaggerGen();
         }
