bcee9155e0725a80cc32e636ce14d129c01498dc
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2022-04-18 23:12:51 +0500

    adjust routing configuration for cordova

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { environment } from '../environments/environment';
 
 const routes: Routes = [];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    imports: [RouterModule.forRoot(routes, { useHash: !environment.browser })],
     exports: [RouterModule],
 })
 export class AppRoutingModule { }
