8e47536167554c863c98f30e99a7a67335de8f91
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2022-04-19 00:46:49 +0500

    set up CORS

--- HwaSrsApi/Startup.cs
+++ HwaSrsApi/Startup.cs
@@ -20,6 +20,24 @@ namespace HwaSrsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("LocalhostNgCredentials", policyBuilder =>
+                {
+                    policyBuilder
+                        .WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .AllowAnyMethod();
+                });
+                options.AddPolicy("AllowAny", policyBuilder =>
+                {
+                    policyBuilder
+                        .AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
             services.AddDbContext<SrsContext>(builder =>
             {
                 builder.UseNpgsql(Configuration["DbConnection"]);
@@ -48,6 +66,8 @@ namespace HwaSrsApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("AllowAny");
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
