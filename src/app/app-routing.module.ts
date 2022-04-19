3403ffc2e8afe3b2974781b10ef393c53f68073d
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2022-04-19 23:16:19 +0500

    set up routing, add courses list to main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { environment } from '../environments/environment';
+import { CoursesComponent } from './components/courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full',
+    },
+    {
+        path: 'courses',
+        component: CoursesComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes, { useHash: !environment.browser })],
