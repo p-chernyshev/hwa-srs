8b36b4a579d2a4b4d2e139f5adf5b07b924cbf0c
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2022-06-05 13:01:09 +0500

    add observable DB methods

--- src/app/services/local/database.service.ts
+++ src/app/services/local/database.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IDBPDatabase, StoreNames, openDB, StoreKey, StoreValue, DBSchema } from 'idb';
+import { Observable, from } from 'rxjs';
 import { Course } from '../../types/course';
 
 interface SrsDbSchema extends DBSchema {
@@ -21,7 +22,7 @@ export class DatabaseService {
         });
     }
 
-    private static async setValue<StoreName extends StoreNames<SrsDbSchema>>(
+    private static async setValueAsync<StoreName extends StoreNames<SrsDbSchema>>(
         storeName: StoreName, key: StoreKey<SrsDbSchema, StoreName>, value: StoreValue<SrsDbSchema, StoreName>,
     ): Promise<void> {
         const transaction = (await DatabaseService.openDb()).transaction(storeName, 'readwrite');
@@ -29,7 +30,7 @@ export class DatabaseService {
         await store.put(value, key);
     }
 
-    private static async deleteValue<StoreName extends StoreNames<SrsDbSchema>>(
+    private static async deleteValueAsync<StoreName extends StoreNames<SrsDbSchema>>(
         storeName: StoreName, key: StoreKey<SrsDbSchema, StoreName>,
     ): Promise<void> {
         const transaction = (await DatabaseService.openDb()).transaction(storeName, 'readwrite');
@@ -37,7 +38,7 @@ export class DatabaseService {
         await store.delete(key);
     }
 
-    private static async getValue<StoreName extends StoreNames<SrsDbSchema>>(
+    private static async getValueAsync<StoreName extends StoreNames<SrsDbSchema>>(
         storeName: StoreName, key: StoreKey<SrsDbSchema, StoreName>,
     ): Promise<StoreValue<SrsDbSchema, StoreName> | undefined> {
         const transaction = (await DatabaseService.openDb()).transaction(storeName, 'readonly');
@@ -45,7 +46,7 @@ export class DatabaseService {
         return store.get(key);
     }
 
-    private static async getKeyValueMap<StoreName extends StoreNames<SrsDbSchema>>(
+    private static async getKeyValueMapAsync<StoreName extends StoreNames<SrsDbSchema>>(
         storeName: StoreName,
     ): Promise<Map<StoreKey<SrsDbSchema, StoreName>, StoreValue<SrsDbSchema, StoreName>>> {
         const transaction = (await DatabaseService.openDb()).transaction(storeName, 'readonly');
@@ -63,22 +64,46 @@ export class DatabaseService {
         return map;
     }
 
+    private static setValue<StoreName extends StoreNames<SrsDbSchema>>(
+        storeName: StoreName, key: StoreKey<SrsDbSchema, StoreName>, value: StoreValue<SrsDbSchema, StoreName>,
+    ): Observable<void> {
+        return from(this.setValueAsync(storeName, key, value));
+    }
+
+    private static deleteValue<StoreName extends StoreNames<SrsDbSchema>>(
+        storeName: StoreName, key: StoreKey<SrsDbSchema, StoreName>,
+    ): Observable<void> {
+        return from(this.deleteValueAsync(storeName, key));
+    }
+
+    private static getValue<StoreName extends StoreNames<SrsDbSchema>>(
+        storeName: StoreName, key: StoreKey<SrsDbSchema, StoreName>,
+    ): Observable<StoreValue<SrsDbSchema, StoreName> | undefined> {
+        return from(this.getValueAsync(storeName, key));
+    }
+
+    private static getKeyValueMap<StoreName extends StoreNames<SrsDbSchema>>(
+        storeName: StoreName,
+    ): Observable<Map<StoreKey<SrsDbSchema, StoreName>, StoreValue<SrsDbSchema, StoreName>>> {
+        return from(this.getKeyValueMapAsync(storeName));
+    }
+
     constructor() {
     }
 
-    public async getCourse(id: Course['id']): Promise<Course | undefined> {
+    public getCourse(id: Course['id']): Observable<Course | undefined> {
         return DatabaseService.getValue('courses', id);
     }
 
-    public async getCourses(): Promise<Map<Course['id'], Course>> {
+    public getCourses(): Observable<Map<Course['id'], Course>> {
         return DatabaseService.getKeyValueMap('courses');
     }
 
-    public async saveCourse(id: Course['id'], course: Course): Promise<void> {
+    public saveCourse(id: Course['id'], course: Course): Observable<void> {
         return DatabaseService.setValue('courses', id, course);
     }
 
-    public async deleteCourse(id: Course['id']): Promise<void> {
+    public deleteCourse(id: Course['id']): Observable<void> {
         return DatabaseService.deleteValue('courses', id);
     }
 }
